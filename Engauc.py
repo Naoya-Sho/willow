d191204ecdea74d918a1521e74adfd26f164f7e0
Naoya Sho <ut10.sho708@gmail.com>
2014-09-12 06:20:03 +0900

    まだ動作おかしいですが更新

--- Engauc.py
+++ Engauc.py
@@ -8,62 +8,69 @@ def session(me):
 
         add(open("Engauc0.html"))
   	
-        take({"client":me})     # Block until the initial setting is ready.
+        take({"tag": "click"})     # Block until the initial setting is ready.
 
-        plnum = peek("#plnum")  # Define variables from initial settings
-        gnum = peek("#gnum") 
-        valuemin = peek("#valuemin")
-        valuemax = peek("#valuemax")
+        plnum = int(peek("#plnum"))  # Define variables from initial settings
+        gnum = int(peek("#gnum"))
+        valuemin = int(peek("#valuemin"))
+        valuemax = int(peek("#valuemax"))
         put({"tag":"toclient","plnum":plnum, "gnum":gnum, "valuemin":valuemin, "valuemax":valuemax}) # Make a dictionary to let clients to start the experiments.
 
         while True:  # Wait until every client reads the instruction.
 
             msg = take({"tag":"readinst"})
   	    pl += 1
-            add("<p>Client %s has finished reading instructions.</p>" % msg["number"])
+            add("<p>Client %s has finished reading instructions.</p>" % msg["number"], "#inst")
 
             if pl == int(plnum):
                 break
             else:
                 continue
 
+        show("#ready")
         msg = take({"client": me}) # Start the main part.
         put({"tag":"ready"})
+        show("#start")
 
         ### main auction part starts from here. can be changed for other types of auctions.
 
         count = 0
-        round = 1
+        round = 0
         price = valuemin - 10
 
-        while True:  
-            put({"tag":"auction", "round":round, "price":price})
+        while True:
 
-            add("<p>Begin round %s. Current price is %s."%round, price)
+            ## Initialize the variables and dictionaries
+            pl = 0
+            count = 0
+            price += 10
+            round += 1
+
+            put({"tag":"auction", "round":round, "price":price})
+            add("<p>Begin round %s. Current price is %s." % (str(round), str(price)))
 
             while True:  
-                msg = take({"tag":"tomonitor"})
+                clchoice = take({"tag":"mychoice"})
                 pl += 1
-                if msg["choice"] == "bid":
+                if clchoice["choice"] == "bid":
                     count += 1
-                add("<p>Client %s has chosen %s." % msg["client"], msg["choice"])
+                add("<p>Client %s has chosen %s." % (clchoice["client"], clchoice["choice"]))
 
                 if pl == int(plnum):
                     break
                 else:
                     continue
 
-            if count <= gnum:
+            if count <= int(gnum):
                 show("#finish")
+                put({"tag":"loop","finish":True}) # To tell the clients that the auction has finished.
                 break
-                put({"tag":"toclient","finish":True}) # To tell the clients that the auction has finished.
-
 
             else:
-                add("<p>There are %s bids for %s goods. Proceed to a next round." % count, gnum)
-                round += 1
-                price = price + 10
-                put({"tag":"toclient","continue":True}) # To tell the clients that the auction has not finished yet.
+                add("<p>There are %s bids for %s goods. Proceed to a next round." % (str(count), str(gnum)))
+                grab({"tag":"auction"})
+                grab({"tag":"loop"})
+                put({"tag":"loop","continue":True}) # To tell the clients that the auction has not finished yet.
                 continue
 
 
@@ -73,11 +80,13 @@ def session(me):
         variables = take({"tag":"toclient"}) # Block until the monitor finishes the itinial setting.
         put(variables) # Recreate the dictionary for other clients
 
-        value = random.randint(variables["valuemin"], variables["valuemax"]) # Calcurate the private value of the goods
+        value = random.randrange(variables["valuemin"], variables["valuemax"], 10) # Calcurate the private value of the goods
 
         show("#start") # Start to show an instruction
         add(variables["gnum"], "#gnum")
-        add(value, "#value")
+        add(value, "#value0")
+        add(value, "#value1")
+        add(value, "#value2")
 
         take({"client":me}) # Block until the client finishes reading instructions
         put({"tag":"readinst", "number":me }) # Let the monitor know that the client is ready
@@ -91,34 +100,36 @@ def session(me):
         put(auction)
 
         show("#auction")
+        add(value, "#value3")
         add(auction["price"],"#price")
 
+
         mychoice = take({"client": me}) # choice in the 1st round
-        put({"tag":"tomonitor", "choice":mychoice["id"], "client":me}) 
+        put({"tag":"mychoice", "choice": mychoice["id"], "client":me}) 
 
-        msg = take({"tag":"toclient"}) # For checking whether proceed or stop
-        put(msg)
+        clloop = take({"tag":"loop"}) # For checking whether to proceed or stop
+        put(clloop)
 
-        while "continue" in msg: # loop for rounds after 1st
+        while "continue" in clloop: # loop for rounds after 1st
             auction = take({"tag":"auction"})
             put(auction)
-            add("<p>The number of bids exeeds the number of goods. Please proceed to a next round.</p>")
-            add("<p>The value of the goods for you is <strong>%s</strong>. The current price is <strong>%s</strong></p>"% value, auction["price"] )
+            add("<p>The number of bids exceeds the number of goods. Please proceed to a next round.</p>")
+            add("<p>The value of the goods for you is <strong>%s</strong>. The current price is <strong>%s</strong></p>" % (str(value), str(auction["price"])))
             add("<p>Please select your action from below.</p>")
             add("<input class='choice' id='bid' value='Bid' type='submit' />")
             add("<input class='choice' id='stop' value='Stop' type='submit' />")
-            mychoice = take({"client": me})
-            put({"tag":"tomonitor", "choice":msg["id"], "client":me})
-            
-            msg = take({"tag":"toclient"}) # For checking whether proceed or stop
-            put(msg)
+            mychoicel = take({"client": me})
+            put({"tag":"mychoice", "choice":mychoicel["id"], "client":me})
+            sleep(3)
+            clloop = take({"tag":"loop"}) # For checking whether to proceed or stop
+            put(clloop)
 
         else:
-            put({"tag":"result", "choice":msg["id"], "client":me}) # Send a result to the monitor
-            if msg["id"] == bid:
+            put({"tag":"result", "choice":mychoicel["id"], "client":me}) # Send a result to the monitor
+            if mychoicel["id"] == "bid":
                 payoff = value - auction["price"]
-                add("<p>You successfully purchased the good for %s Yen. </p>"% auction["price"] )
-                add("<p>Your final payoff is %s .</p>"% payoff)
+                add("<p>You successfully purchased the good for %s Yen. </p>"% str(auction["price"]))
+                add("<p>Your final payoff is %s .</p>"% str(payoff))
 
             else:
                 add("<p>You did not purchase the good.</p>")
